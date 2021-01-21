16958c0e8a76084395e3e68fde5dd128dec43821
dev-LOVE-per <77622006+dev-LOVE-per@users.noreply.github.com>
2021-01-21 09:20:07 +0530

    port configured to process.env.port

--- src/config/vars.js
+++ src/config/vars.js
@@ -8,7 +8,6 @@ require('dotenv-safe').load({
 
 module.exports = {
   env: process.env.NODE_ENV,
-  port: process.env.PORT,
   jwtSecret: process.env.JWT_SECRET,
   jwtExpirationInterval: process.env.JWT_EXPIRATION_MINUTES,
   mongo: {
