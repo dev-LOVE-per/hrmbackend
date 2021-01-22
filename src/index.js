d8785c31f285584c23293f1bbaef527a074a19f7
dev-LOVE-per <77622006+dev-LOVE-per@users.noreply.github.com>
2021-01-22 09:12:06 +0530

    test for heroku

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 // create an express app
-const express = require("express")
-const app = express()
+const express = require('express');
+
+const app = express();
 
 // use the express-static middleware
-app.use(express.static("public"))
+app.use(express.static('public'));
 
 // define the first route
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
-})
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>');
+});
 
 // start the server listening for requests
-app.listen(process.env.PORT || 3001, 
-	() => console.log("Server is running..."));
+app.listen(
+  process.env.PORT || 3001,
+  () => console.log('Server is running...'),
+);
